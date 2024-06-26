ed6f394fd298fc83fb96da4245291262046d2949
Luca324 <tanya.mosol@yandex.ru>
2024-06-26 11:17:58 +0300

    animations and hovers
    выплывающее меню, hover-ы

--- js/MYN3.js
+++ js/MYN3.js
@@ -1,19 +1,22 @@
 // при вводе R и E значение выводится на схему
 const inputListener = function(id) {
-  console.log('*in inputListener* id = ', id)
   document.querySelector('#'+ id).addEventListener('input', () => {
-    console.log('input ' + id)
     $('#' + id + '_shema_value').text($("#" + id).val())
  
   })
-  console.log('#' + id + 'shema_value'+ ' inputListener added. to ' +"#" + id)
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const inputIds = ['R1', 'R2', 'R3', 'E1', 'E2', 'E3']
+
+  document.querySelector('.menubutton').addEventListener('click', () => {
+    console.log('menubutton clicked')
+    document.querySelector('nav').classList.toggle('collapsed');
+    document.querySelector('.menubutton').classList.toggle('collapsed');
+  });
+
+  const inputIds = ['R1', 'R2', 'R3', 'R4', 'E1', 'E2', 'E3']
   for (id in inputIds) {
     inputListener(inputIds[id])
-    console.log('id = ' + id, 'inputIds[id] = ' + inputIds[id])
   }
 
   document.querySelector('form').addEventListener("submit", (event) => {
@@ -23,22 +26,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const g1 = +(1/($("#R1").val()+$("#r1").val())).toFixed(3)
     const g2 = +(1/($("#R2").val()+$("#r2").val())).toFixed(3)
     const g3 = +(1/($("#R3").val()+$("#r3").val())).toFixed(3)
-    console.log('g1, g2, g3 = ', g1, g2, g3)
-    console.log('counter1,2,3 = ', $("#counter1").val(), $("#counter2").val(), $("#counter3").val())
 
     //U 
     const E1_val = $("#E1").val()*$("#counter1").val()
     const E2_val = $("#E2").val()*$("#counter2").val()
     const E3_val = $("#E3").val()*$("#counter3").val()
     const U = ((E1_val*g1 + E2_val*g2 + E3_val*g3)/(g1+g2+g3)).toFixed(2)
-    console.log('E1_val, E2_val, E3_val', E1_val, E2_val, E3_val)
-    console.log('U after = ', U)
 
     //ток для каждой ветви
     const I1 = ((E1_val-U)*g1).toFixed(2)
     const I2 = ((E2_val-U)*g2).toFixed(2)
     const I3 = ((E3_val-U)*g3).toFixed(2)
-    console.log('I1, I2, I3 =', I1, I2, I3)
 
 
     $('.equals').css('display', 'inline-block')
@@ -48,13 +46,11 @@ document.addEventListener("DOMContentLoaded", function () {
       if (isvariable === false) {
         $('.'+id+'_formula').html($('#'+id).val())
       } else {
-        console.log('_')
         $('.'+id+'_formula').html(variable)
       }
     }
     ids = ['R1', 'R2', 'R3', 'r1', 'r2', 'r3']
     for (i in ids) {
-      console.log('i =', ids[i])
       myInnerHTML(ids[i])
     }
     myInnerHTML('g1', true, g1)
